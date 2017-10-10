f580403bbed7c4987091a82937fd50e32360aeee
Your Name Comes Here <you@yourdomain.example.com>
2017-10-10 10:22:21 +0800

    lab1

--- lab1/GraphVizTest/src/GraphVizTest/GraphVizTest.java
+++ lab1/GraphVizTest/src/GraphVizTest/GraphVizTest.java
@@ -1,5 +1,5 @@
 package GraphVizTest;
-//fdsfsd
+//dsadk
 import java.io.IOException;
 
 public class GraphVizTest {
--- lab1/com.runoob.test/src/com/runoob/test/helloword.java
+++ lab1/com.runoob.test/src/com/runoob/test/helloword.java
@@ -3,7 +3,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//dklsad
 public class helloword {
 	static BufferedReader keyboard = new
 			BufferedReader(new InputStreamReader(System.in));
