e47974dc36826f3ae6b36fd88875379fdac41b3e
Your Name Comes Here <you@yourdomain.example.com>
2017-10-10 09:35:19 +0800

    lab1

---
+++ lab1/GraphVizTest/src/GraphVizTest/GraphVizTest.java
@@ -0,0 +1,21 @@
+package GraphVizTest;
+
+import java.io.IOException;
+
+public class GraphVizTest {
+    public static void main(String[] args) {
+        GraphViz gViz=new GraphViz("C:\\Users\\Baratta-Desktoop\\Desktop\\eee", "D:\\Graphviz2.38\\bin\\dot.exe");
+        gViz.start_graph();
+        gViz.addln("A->B;");
+        gViz.addln("A->C;");
+        gViz.addln("C->B;");
+        gViz.addln("B->D;");
+        gViz.addln("C->E;");
+        gViz.end_graph();
+        try {
+            gViz.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
